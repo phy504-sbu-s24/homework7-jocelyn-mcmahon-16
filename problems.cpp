472f822f858591ea14df94ffb42e5975d09d0866
Jocelyn McMahon <jocelyn.mcmahon@stonybrook.edu>
2024-04-29 09:47:34 -0400

    Main file for the homeowork

---
+++ problems.cpp
@@ -0,0 +1,37 @@
+#include <cmath>
+#include <iostream>
+#include <random>
+#include <iomanip>
+
+#include "integrate.H"
+
+int main() {
+    // so I just wrote out the numbers that we want to use as N, but I also have commented out a while loop
+    // that will actually double the value of N so to get those same values.
+
+    const std::vector<int> N = {8, 16, 32, 64, 128, 256, 512, 1024, 2048};
+    // int N_max = 4096;
+    // int N = 8;
+
+    std::cout << "First Integral: e^-(x^2)" << std::endl;
+    std::cout << std::setw(15) << "N" << std::setw(15) <<  "Trap" << std::setw(15) << "MC" << std::endl;
+    
+    for (int i = 0; i < N.size(); i++) {
+        std::cout << std::setw(15) << N[i] << std::setw(15) << trap_int(gauss, -5, 5, N[i]) << std::setw(15) << mc_int(gauss, -5, 5, N[i]) << std::endl;
+    }
+    // while (N <= N_max) {
+    //     std::cout << std::setw(15) << N << std::setw(15) << trap_int(gauss, -5, 5, N) << std::setw(15) << mc_int(gauss, -5, 5, N) << std::endl;
+    //     N *= 2;
+    // }
+    
+
+    std::cout << "Second Integral: sin^2(1/(x(2-x) + e))" << std::endl;
+    std::cout << std::setw(15) << "N" << std::setw(15) <<  "Trap" << std::setw(15) << "MC" << std::endl;
+    for (int i = 0; i < N.size(); i++) {
+        std::cout << std::setw(15) << N[i] << std::setw(15) << trap_int(sin2, 0, 2, N[i]) << std::setw(15) << mc_int(sin2, 0, 2, N[i]) << std::endl;
+    }
+    // while (N <= N_max) {
+    //     std::cout << std::setw(15) << N << std::setw(15) << trap_int(sin2, 0, 2, N) << std::setw(15) << mc_int(sin2, 0, 2, N) << std::endl;
+    //     N *= 2;
+    // }
+}
