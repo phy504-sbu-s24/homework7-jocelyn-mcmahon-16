d8334879c7fca3ccd0f6da1a46383315cc82268b
Jocelyn McMahon <jocelyn.mcmahon@stonybrook.edu>
2024-04-29 09:52:59 -0400

    Have two methods of doing the loop for the values of N. I left in a vector of N's and a for loop. There is also a while loop that should work

--- problems.cpp
+++ problems.cpp
@@ -9,10 +9,13 @@ int main() {
     // so I just wrote out the numbers that we want to use as N, but I also have commented out a while loop
     // that will actually double the value of N so to get those same values.
 
-    const std::vector<int> N = {8, 16, 32, 64, 128, 256, 512, 1024, 2048};
-    // int N_max = 4096;
-    // int N = 8;
 
+    const std::vector<int> N = {8, 16, 32, 64, 128, 256, 512, 1024, 2048};
+    // int N = 8;
+    // int N_max = 4096;
+    
+
+    // don't comment these two lines
     std::cout << "First Integral: e^-(x^2)" << std::endl;
     std::cout << std::setw(15) << "N" << std::setw(15) <<  "Trap" << std::setw(15) << "MC" << std::endl;
     
@@ -24,12 +27,15 @@ int main() {
     //     N *= 2;
     // }
     
-
+    // don't comment these two lines
     std::cout << "Second Integral: sin^2(1/(x(2-x) + e))" << std::endl;
     std::cout << std::setw(15) << "N" << std::setw(15) <<  "Trap" << std::setw(15) << "MC" << std::endl;
+    
     for (int i = 0; i < N.size(); i++) {
         std::cout << std::setw(15) << N[i] << std::setw(15) << trap_int(sin2, 0, 2, N[i]) << std::setw(15) << mc_int(sin2, 0, 2, N[i]) << std::endl;
     }
+    
+    // N = 8;
     // while (N <= N_max) {
     //     std::cout << std::setw(15) << N << std::setw(15) << trap_int(sin2, 0, 2, N) << std::setw(15) << mc_int(sin2, 0, 2, N) << std::endl;
     //     N *= 2;
